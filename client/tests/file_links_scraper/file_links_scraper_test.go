335c756bb6cd023e5518851c4b54da1626d2d9f5
Constantin Plotkin <kudrikkk@yandex.ru>
2022-07-03 21:59:35 +0200

    dsds

--- client/tests/file_links_scraper/file_links_scraper_test.go
+++ client/tests/file_links_scraper/file_links_scraper_test.go
@@ -2,16 +2,50 @@ package file_links_scraper
 
 import (
 	"acronis/file_links_scraper"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
-	"os"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestGetLinks(t *testing.T) {
 
-	httpClient := http.Client{}
-	mainFileLinksScraper := file_links_scraper.New(httpClient, os.Getenv("FILE_SERVER_URL"))
+	expectedLinks := []string{
+		"file1",
+		"file2",
+	}
 
-	assert.NotEmpty(t, mainFileLinksScraper.GetLinks())
+	ts := EmulateFileServer(expectedLinks)
+
+	mainFileLinksScraper := file_links_scraper.New(http.Client{}, ts.URL)
+
+	links := mainFileLinksScraper.GetLinks()
+
+	assert.NotEmpty(t, links)
+
+	for i, link := range links {
+		assert.Equal(t, ts.URL+"/"+expectedLinks[i], link)
+	}
+}
+
+func TestGetLinksEmpty(t *testing.T) {
+
+	ts := EmulateFileServer([]string{})
+
+	mainFileLinksScraper := file_links_scraper.New(http.Client{}, ts.URL)
+
+	assert.Empty(t, mainFileLinksScraper.GetLinks())
+}
+
+func EmulateFileServer(links []string) *httptest.Server {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "<html><body><pre>")
+		for _, link := range links {
+			fmt.Fprintln(w, "<a href='"+link+"'>some name</a>")
+		}
+		fmt.Fprintln(w, "</pre></body></html>")
+	}))
+
+	return ts
 }
